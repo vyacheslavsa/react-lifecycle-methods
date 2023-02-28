5e976b9c3a80df1ee2b58ff2af51ea3658a67235
VyacheslavSay <viacheslav.saiapin@yandex.ru>
2023-02-28 19:53:05 +0300

    create class Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {//отрабатывает 1 раз при создании компонента
+        console.clear()
+        console.log('CONSTRUCTOR')
+        super();
+        this.state = {
+            value: 0
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {//срабатывает при каждом перерендере
+        //можно отслеживать изменения props и присваивать state value значения arg
+        console.log('getDerivedStateFromProps')
+        return null
+        //return ({value: props.arg}) чтобы присовоить value значение args
+    }
+
+    componentDidMount() {//срабатывает после встраивания в дом дерево
+        console.log('componentDidMount')
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {//компонент обновлен
+        console.log('componentDidUpdate', prevProps, prevState)
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {//срабатывает при офновлении state после метода getDerivedStateFromProps
+        console.log('shouldComponentUpdate', nextProps, nextState)
+        return true
+    }
+
+    buttonHandler = () => {
+        this.setState({value: this.state.value + 1})
+    }
+
+    componentWillUnmount() {//срабатывает перед размонтированием компонента
+        console.log('componentWillUnmount')
+    }
+    //DEPRECATED METHODS
+    //************************
+    //componentWillMount
+    //componentWillUpdate
+    //componentWillReceiveProps
+    //************************
+
+    render() {
+        console.log('render')
+        return (
+            <>
+                <div>React Component App</div>
+                <div>{this.state.value}</div>
+                <button onClick={this.buttonHandler}>inc value</button>
+            </>
+        )
+    }
 }
 
-export default App;
+export default App
+
+
+//mounting
+//***************************
+// CONSTRUCTOR
+// getDerivedStateFromProps
+// render
+// componentDidMount
+//***************************
+
+//update
+//***************************
+// getDerivedStateFromProps
+// shouldComponentUpdate {arg: 10} {value: 1}
+// render
+// componentDidUpdate {arg: 10} {value: 0}
+//***************************
+
+//mounting
+//***************************
+//componentWillUnmount
+//***************************
